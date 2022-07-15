8e7563ec241bf39f5dafb519e7a3df8fb8b448be
ramanverma2k <ramanverma4183@gmail.com>
2022-07-15 11:57:37 +0530

    routes/home: add greeting and intro section

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,17 @@
+import style from './Home.module.css';
+
 function Home() {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className={style.main}>
+      <h1 className={style.heading}>Hey, I&apos;m Lorem Ipsum</h1>
+      <p className={style.intro}>
+        A passionate developer based out of Lucknow, India. I create pixel perfect full stack web
+        &amp; mobile applications. I also have a passion for trying out new technologies and
+        building things that people love.
+        <br />
+        <br />
+        Come watch me fiddle around with Flutter, TypeScript, &amp; Go on Twitch.
+      </p>
     </main>
   );
 }
