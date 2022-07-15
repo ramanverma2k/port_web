74c3ef52842cde59a58aa2bbac9fdbd479d41ff6
ramanverma2k <ramanverma4183@gmail.com>
2022-07-15 10:37:06 +0530

    App: routes: error route

--- src/routes/Error.tsx
+++ src/routes/Error.tsx
@@ -1,7 +1,11 @@
+import style from './Error.module.css';
+import ErrorGo from '../assets/error_go.svg';
+
 function ErrorRoute() {
   return (
-    <main>
-      <h1>Error</h1>
+    <main className={style.main}>
+      <h1 className={style.heading}>Oops!</h1>
+      <img className={style.image} src={ErrorGo} alt="error gopher" />
     </main>
   );
 }
